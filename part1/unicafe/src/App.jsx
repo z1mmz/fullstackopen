cd250acf7ed89086852e1c48d5a96ef15df0e658
Wolf ZImmermann <wolfz@live.com>
2024-10-15 16:35:11 +1100

    average calculation

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -15,19 +15,50 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average,setAverage] = useState(0)
+
+  const handleGood = () =>{
+ 
+    const updatedGood = good+1;
+    setGood(updatedGood);
+    const newTotal = updatedGood + neutral + bad
+    setTotal(newTotal);
+    const newAverage = (updatedGood - bad)/newTotal
+    setAverage(newAverage)
+  }
+  const handleNeutral = () =>{
+    const updatedNeutral= neutral+1;
+    setNeutral(updatedNeutral);
+
+    const newTotal = updatedNeutral + good + bad;
+    setTotal(newTotal);
+    const newAverage = (good - bad)/newTotal
+    setAverage(newAverage)
+  }
+  const handleBad = () =>{
+    const updatedBad = bad+1;
+    setBad(updatedBad);
+    const newTotal = neutral + good + updatedBad;
+    setTotal(newTotal);
+    const newAverage = (good - updatedBad)/newTotal
+    setAverage(newAverage)
+  }
 
 
   return (
     <div>
       <h1>Give feedback</h1>
-      <button onClick={() => setGood(good+1)}>good</button>
-      <button onClick={() => setNeutral(neutral+1)}>neutral</button>
-      <button onClick={() => setBad(bad+1)}>bad</button>
+      <button onClick={() => handleGood()}>good</button>
+      <button onClick={() => handleNeutral()}>neutral</button>
+      <button onClick={() => handleBad()}>bad</button>
       <h1>Statistics</h1>
       <Statistics name={"good"} count={good}/>
       <Statistics name={"neutral"} count={neutral}/>
       <Statistics name={"bad"} count={bad}/>
-      
+      <Statistics name={"total"} count={total}/>
+      <Statistics name={"total"} count={total}/>
+      <p>{average}</p>
     </div>
   )
 }
