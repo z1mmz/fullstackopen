861ff7b0734ee3f6d8e5b5a204643a51df27edd4
Wolf ZImmermann <wolfz@live.com>
2024-10-15 20:52:46 +1100

    unicafe step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 
-const Statistics = ({name,count}) =>{
-  
+const Statistics = ({counts,avg,pctPos}) =>{
+  console.log(counts);
+  const countsDisplay = Object.keys(counts).map((keys)=>{ return <p>{keys} {counts[keys]}</p>})
+  console.log(countsDisplay)
   return(
     <div>
-      <p>{name} {count}</p>
-
+      {countsDisplay}
+      <p>average {avg()}</p>
+      <p>positive {pctPos()*100}%</p>
     </div>
   )
 }
@@ -18,31 +21,40 @@ const App = () => {
   const [total, setTotal] = useState(0)
   const [average,setAverage] = useState(0)
 
+  const counts = {"good":good,"neutral":neutral,"bad":bad}
+
+  const calculatePos = () => {
+    if (total>0){
+      return good/total
+    }else{
+      return 0
+    }
+  }
+  const calculateAVG = () =>{
+    if(total){
+      return (good - bad)/total
+    }else{
+      return 0
+    }
+  }
+
   const handleGood = () =>{
- 
     const updatedGood = good+1;
     setGood(updatedGood);
     const newTotal = updatedGood + neutral + bad
     setTotal(newTotal);
-    const newAverage = (updatedGood - bad)/newTotal
-    setAverage(newAverage)
   }
   const handleNeutral = () =>{
     const updatedNeutral= neutral+1;
     setNeutral(updatedNeutral);
-
     const newTotal = updatedNeutral + good + bad;
     setTotal(newTotal);
-    const newAverage = (good - bad)/newTotal
-    setAverage(newAverage)
   }
   const handleBad = () =>{
     const updatedBad = bad+1;
     setBad(updatedBad);
     const newTotal = neutral + good + updatedBad;
     setTotal(newTotal);
-    const newAverage = (good - updatedBad)/newTotal
-    setAverage(newAverage)
   }
 
 
@@ -53,12 +65,13 @@ const App = () => {
       <button onClick={() => handleNeutral()}>neutral</button>
       <button onClick={() => handleBad()}>bad</button>
       <h1>Statistics</h1>
-      <Statistics name={"good"} count={good}/>
+      <Statistics counts={counts} avg={() => calculateAVG()} pctPos={() => calculatePos()} />
+      {/* <Statistics name={"good"} count={good}/>
       <Statistics name={"neutral"} count={neutral}/>
       <Statistics name={"bad"} count={bad}/>
       <Statistics name={"total"} count={total}/>
-      <Statistics name={"total"} count={total}/>
       <p>{average}</p>
+      <p>{calculateAVG()}</p> */}
     </div>
   )
 }
