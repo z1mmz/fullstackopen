62996099e14a3e0a430a15e8cdc19c008434010a
Wolf Zimmermann <wolf.zimmermann@educaiton.vic.gov.au>
2025-04-01 14:02:28 +1100

    Part 3 Frontend build serve from express

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -7,7 +7,7 @@ morgan.token("body",function (req, res){return JSON.stringify(req.body)})
 
 app.use(morgan("tiny :body"))
 app.use(cors())
-
+app.use(express.static('dist'))
 app.use(express.json())
 let phonebook = [
     { 
