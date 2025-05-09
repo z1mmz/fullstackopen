79a898ed53394ed16ecb5ec2d23c9353bff0ccaa
Wolf ZImmermann <wolfz@live.com>
2025-05-09 10:34:28 +1000

    Part 3 3.20 phone number validation

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -18,8 +18,18 @@ const personSchema = new mongoose.Schema({
         minLength: 3,
         required: true
     },
-    number: String,
-})
+    number: {
+        type: String,
+        validate: {
+            validator: function(v){
+                return v.replace(/[^0-9]/g,'').length >=8 & /\d{2,3}\-[\d\-]*/g.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number!`
+          },
+          required: [true, 'User phone number required']
+
+        }
+    })
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
