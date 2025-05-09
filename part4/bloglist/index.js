3c412d017ce1b2ac2d87e6d11ae21d29183a2de8
Wolf ZImmermann <wolfz@live.com>
2025-05-09 13:53:28 +1000

    Part4 created blogsrouter and added eslint

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -2,25 +2,12 @@ const express = require('express')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
-const Blog = require('./models/blog')
+
+const blogsRouter = require('./controllers/blogs')
 const app = express()
 
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
+app.use('/api/blogs', blogsRouter)
 app.listen(config.PORT, () => {
     logger.info(`Server running on port ${config.PORT}`)
 })
--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -2,21 +2,28 @@ const mongoose = require('mongoose')
 const config = require('../utils/config')
 
 const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number,
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
 })
 
 
 mongoose.connect(config.MONGODB_URI).then(
-    () => {
-        console.log('connected to mongo db')
-    }
-    ).catch(
-    error => {
-        console.log('error connecting to MongoDB:', error.message)
-    }
-    )
+  () => {
+    console.log('connected to mongo db')
+  }
+).catch(
+  error => {
+    console.log('error connecting to MongoDB:', error.message)
+  }
+)
 
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 module.exports = mongoose.model('Blog', blogSchema)
