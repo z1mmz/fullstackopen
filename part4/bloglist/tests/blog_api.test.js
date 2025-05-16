1890a3fe774efde329350d7fccee0f927502b22f
Wolf ZImmermann <wolfz@live.com>
2025-05-16 14:11:36 +1000

    part 4 4.10

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -30,6 +30,25 @@ test('all blogs are returned', async () => {
   })
 })
 
+describe("blog posting test", () => {
+    test('test blog is created', async () =>{
+        const blogsBefore = await api.get('/api/blogs')
+
+        const blog_payload = {
+            title: 'add one',
+            author: 'adder',
+            url: 'www.testplus1.com',
+            likes: 101,
+          }
+        const response = await api.post('/api/blogs').send(blog_payload)
+        assert.strictEqual(response.status,201)
+        const blogsAfter = await api.get('/api/blogs')
+        assert.strictEqual(blogsBefore.body.length+1,blogsAfter.body.length)
+    })
+
+
+})
+
 describe("blog field test", () => {
    test('id is defined', async () => {
     const response = await api.get('/api/blogs')
