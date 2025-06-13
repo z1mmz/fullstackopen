1d004f7cd08ebbaac6e7f3a5ddeb513463aff488
Wolf ZImmermann <wolfz@live.com>
2025-06-13 14:10:30 +1000

    Part 4 4.18 Implement token-based authentication

--- part4/bloglist/models/user.js
+++ part4/bloglist/models/user.js
@@ -6,6 +6,10 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique:true
     },
+    passwordHash:{
+        type: String,
+        required: true,
+    },
     name: String,
     blogs: [
         {
