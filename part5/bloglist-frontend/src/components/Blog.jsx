13ea1ef2b8adccbde79ac10df32f488e0caa8393
Wolf ZImmermann <wolfz@live.com>
2025-09-05 15:14:54 +1000

    part 5 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import { useState } from "react"
+import Togglable from "./togglable"
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  
+  const [detailsVisible, setDetailsVisible] = useState(false)
+  
+
+  const toggleVisibility = () => {
+    setDetailsVisible(!detailsVisible)
+  }
+
+  
+  const showWhenVisible = { display: detailsVisible ? '' : 'none' }
+  return (
+  <div style={blogStyle}>
+    <div>{blog.title} {blog.author} <button onClick={() => (toggleVisibility())}>{!detailsVisible? "view":"close"}</button></div> 
+    <div style={showWhenVisible}>
+      <div>{blog.url}</div>
+      <div>likes {blog.likes} <button>like</button></div>
+      <div>{blog.user.name}</div>
+    </div>
   </div>  
 )
-
+}
 export default Blog
