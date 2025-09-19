c98bd648f33fc6044001a8093cfdea57dae61207
Wolf ZImmermann <wolfz@live.com>
2025-09-19 16:59:06 +1000

    5.16

--- part5/bloglist-frontend/src/components/BlogForm.jsx
+++ part5/bloglist-frontend/src/components/BlogForm.jsx
@@ -19,16 +19,19 @@ const BlogForm = ({ handleBlogSubmit }) => {
       <h1>Create new</h1>
       <form onSubmit={handleSubmit}>
         <div>
-          <label>Title:</label>
-          <input type='text' value={title} onChange={({ target }) => setTitle(target.value)} />
+          <label>Title:
+            <input type='text' value={title} onChange={({ target }) => setTitle(target.value)} />
+          </label>
         </div>
         <div>
-          <label>Author:</label>
-          <input type='text' value={author} onChange={({ target }) => setAuthor(target.value)} />
+          <label>Author:
+            <input type='text' value={author} onChange={({ target }) => setAuthor(target.value)} />
+          </label>
         </div>
         <div>
-          <label>Url:</label>
-          <input type='text' value={url} onChange={({ target }) => setUrl(target.value)} />
+          <label>Url:
+            <input type='text' value={url} onChange={({ target }) => setUrl(target.value)} />
+          </label>
         </div>
         <button type='submit'>Post</button>
       </form>
---
+++ part5/bloglist-frontend/src/components/BlogForm.test.jsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react'
+import BlogForm from './BlogForm'
+
+import userEvent from '@testing-library/user-event'
+import { test } from 'vitest'
+
+test('calls the event handler it received as props with the right details when a new blog is created', async () => {
+  const createBlog = vi.fn()
+  render(<BlogForm handleBlogSubmit={createBlog} />)
+  const test_user = userEvent.setup()
+
+  const inputTitle = screen.getByLabelText('Title:')
+  const inputAuthor = screen.getByLabelText('Author:')
+  const inputUrl = screen.getByLabelText('Url:')
+  const sendButton = screen.getByText('Post')
+
+  await test_user.type(inputTitle, 'testing a form...')
+  await test_user.type(inputAuthor, 'tester')
+  await test_user.type(inputUrl, 'http://test.com')
+  await test_user.click(sendButton)
+  console.log('mock', createBlog.mock.calls)
+  expect(createBlog.mock.calls).toHaveLength(1)
+  expect(createBlog.mock.calls[0][0].title).toBe('testing a form...')
+  expect(createBlog.mock.calls[0][0].author).toBe('tester')
+  expect(createBlog.mock.calls[0][0].url).toBe('http://test.com')
+})
