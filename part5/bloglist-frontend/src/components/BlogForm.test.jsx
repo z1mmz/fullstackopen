c937c103d867e9ee62a0a06b7677e060d293ebdf
Wolf ZImmermann <wolfz@live.com>
2025-10-15 20:28:31 +1100

    Add Playwright testing framework and initial test cases

--- part5/bloglist-frontend/src/components/BlogForm.test.jsx
+++ part5/bloglist-frontend/src/components/BlogForm.test.jsx
@@ -2,7 +2,6 @@ import { render, screen } from '@testing-library/react'
 import BlogForm from './BlogForm'
 
 import userEvent from '@testing-library/user-event'
-import { test } from 'vitest'
 
 test('calls the event handler it received as props with the right details when a new blog is created', async () => {
   const createBlog = vi.fn()
